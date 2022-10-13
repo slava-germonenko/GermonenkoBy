1c4329f615a08f35d6cec59afb0737c44778cfe6
Slava Germonenko <slava.germonenko@gmail.com>
2022-10-13 17:15:38 +0200

    Implemented start sessions core logic and infrastructure.

---
+++ src/GermonenkoBy.Sessions.Core/UserSessionsService.cs
@@ -0,0 +1,60 @@
+using Microsoft.EntityFrameworkCore;
+
+using GermonenkoBy.Common.Domain.Exceptions;
+using GermonenkoBy.Sessions.Core.Dtos;
+using GermonenkoBy.Sessions.Core.Models;
+using GermonenkoBy.Sessions.Core.Repositories;
+
+namespace GermonenkoBy.Sessions.Core;
+
+public class UserSessionsService
+{
+    private readonly IUsersRepository _repository;
+
+    private readonly SessionsContext _context;
+
+    public UserSessionsService(IUsersRepository repository, SessionsContext context)
+    {
+        _repository = repository;
+        _context = context;
+    }
+
+    public async Task<UserSession> StartOrRefreshSessionAsync(StartUserSessionDto sessionDto)
+    {
+
+
+        var session = await _context.UserSessions.FirstOrDefaultAsync(
+            s => s.DeviceId == sessionDto.DeviceId && s.UserId == sessionDto.UserId
+        );
+
+        if (session is null)
+        {
+            await EnsureUserExistsAsync(sessionDto.UserId);
+            session = new()
+            {
+                DeviceId = sessionDto.DeviceId,
+                DeviceName = sessionDto.DeviceName,
+                UserId = sessionDto.UserId
+            };
+        }
+
+        session.ExpireDate = sessionDto.ExpireDate;
+
+        _context.UserSessions.Update(session);
+        await _context.SaveChangesAsync();
+
+        return session;
+    }
+
+    private async Task EnsureUserExistsAsync(int userId)
+    {
+        try
+        {
+            await _repository.GetUserAsync(userId);
+        }
+        catch (NotFoundException e)
+        {
+            throw new CoreLogicException(e.Message);
+        }
+    }
+}
