f4ce7a49506ceab58dc816107e09e2ccd003fe24
Slava Germonenko <slava.germonenko@gmail.com>
2022-10-27 09:22:25 +0200

    Implemented authorization contracts

---
+++ src/GermonenkoBy.Authorization.Infrastructure/Contracts/Clients/HttpUsersClient.cs
@@ -0,0 +1,42 @@
+using GermonenkoBy.Authorization.Core.Contracts.Clients;
+using GermonenkoBy.Authorization.Core.Models;
+using GermonenkoBy.Common.Domain.Exceptions;
+using GermonenkoBy.Common.Web.Http;
+using GermonenkoBy.Common.Web.Responses;
+
+namespace GermonenkoBy.Authorization.Infrastructure.Contracts.Clients;
+
+public class HttpUsersClient : IUsersClient
+{
+    private readonly HttpClientFacade _httpClient;
+
+    public HttpUsersClient(HttpClient httpClient)
+    {
+        _httpClient = new HttpClientFacade(httpClient);
+    }
+
+    public async Task<User?> GetUserAsync(string emailAddress)
+    {
+        var queryParams = new Dictionary<string, string?>
+        {
+            { "emailAddress", emailAddress }
+        };
+        var users = await _httpClient.GetAsync<ContentListResponse<User>>("api/users", queryParams);
+        return users.Data?.FirstOrDefault(u => u.EmailAddress == emailAddress);
+    }
+
+    public async Task<bool> ValidatePasswordIsValid(int userId, string password)
+    {
+        try
+        {
+            var url = $"api/users/{userId}/password-validation";
+            // It returns 204 if password is valid
+            await _httpClient.PostAsync(url, body: new { password });
+            return true;
+        }
+        catch (CoreLogicException)
+        {
+            return false;
+        }
+    }
+}
